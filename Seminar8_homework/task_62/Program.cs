a38fedca869677e896c6868e7809cbcb35d2f535
Roman V <the1expi6@gmail.com>
2023-09-24 12:08:27 +0300

    Added not very good algorithm for task_62

--- Seminar8_homework/task_62/Program.cs
+++ Seminar8_homework/task_62/Program.cs
@@ -7,9 +7,8 @@
 5 2 6 7
 Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 */
-//
-//          НЕ ДОДЕЛАНО К ДЕДЛАЙНУ!!! 
-//
+//Доделано после дедлайна
+
 int InputNum (string message){
     Console.WriteLine(message);
     return int.Parse(Console.ReadLine()!);
@@ -25,6 +24,7 @@ void Fill2dArray(int[,] array, int size)
     int count = 0;
     for (int i = 0; i < array.GetLength(0); i++)
         for (int j = 0; j < array.GetLength(1); j++)
+        {
             if (i == 0)
             {
                 array[i,j] = ++count;
@@ -33,48 +33,35 @@ void Fill2dArray(int[,] array, int size)
             {
                 if (j == array.GetLength(1) - 1)
                 {
-                    array[i,j] = count;
+                    array[i,j] = ++count;
                 }
                 else
                 {
                     array[i,j] = size - array.GetLength(1) + j;
-                    count++;
                 }
             }
             else if (i == 2)
             {
                 if (j == array.GetLength(1) - 1)
                 {
-                    array[i,j] = count;
+                    array[i,j] = ++count;
                 }
-                else
+                else if (j == 0)
                 {
-                    array[i,j] = size - array.GetLength(1) + j;
-                    count++;
+                    array[i,j] = size - count;
+                }
+                else 
+                {
+                    array[i,j] = size - (j-1);
                 }
             }
+            if (i == array.GetLength(0)-1)
+            {
+                //Console.WriteLine(k);
+                array[i,array.GetLength(1)-1 - j] = ++count;
+            }
+        }
             
-}
-
-int MinRowSum(int[,] array)
-{
-    int[] temp = new int[array.GetLength(1)];
-    int min = 0;
-
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-            temp[i] += array[i,j]; 
-        if (temp[i] < temp[min]) 
-            min = i;
-    }
-    // for (int k = 1; k < temp.Length; k++)
-    //     if (temp[k] < temp[min]) 
-    //         min = k;
-
-    Console.WriteLine("Одномерный массив сумм: ");
-    PrintArray(temp);
-    return min;
 }
 
 void Print2DArray(int [,] array)
@@ -104,4 +91,3 @@ int[,] arr = Create2DArray(rows, columns);
 Fill2dArray(arr, size);
 Console.WriteLine("Двумерный массив: ");
 Print2DArray(arr);
-Console.WriteLine($"Наименьшая сумма: {MinRowSum(arr)+1} строка");
